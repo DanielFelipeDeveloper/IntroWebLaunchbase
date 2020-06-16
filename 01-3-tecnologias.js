1ac0dc89dac43b6aa0d5d006be1ae03cbb5098af
DanielFelipeDeveloper <danielfelipedeveloper@gmail.com>
2020-06-16 10:25:49 -0400

    Desafio 1-3

--- 01-3-tecnologias.js
+++ 01-3-tecnologias.js
@@ -4,6 +4,24 @@ const usuarios = [
     { nome: "Marcela", tecnologias: ["C#", "SQL"] }
 ]
 
-for (usuario of usuarios) {
-    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias}`)
-}
+// for (let usuario of usuarios) {
+//     console.log(`${usuario.nome} trabalha com ${usuario.tecnologias}`)
+// }
+
+function checaSeUsuarioUsaCSS(usuario) {
+    for (let tecnologia of usuario.tecnologias) {
+        if (tecnologia == 'Css') {
+            return true
+        }
+    }
+
+    return false
+  }
+
+  for (let i = 0; i < usuarios.length; i++) {
+        const usuarioUsaCSS = checaSeUsuarioUsaCSS(usuarios[i])
+
+        if (usuarioUsaCSS) {
+            console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
+        }
+  }
